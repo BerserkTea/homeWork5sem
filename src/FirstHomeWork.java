7b124f1cb32f772d960fad2d6ebcfe4a930a6e79
Andronov Ivan (Ber Tea) <andronoviv4@gmail.com>
2023-09-22 23:00:36 +0300

    Added Sorted out, not make private methods, just make all in getAllSorted method, hope its work. wanna sleep

--- src/FirstHomeWork.java
+++ src/FirstHomeWork.java
@@ -1,6 +1,16 @@
-import java.util.*;
+// Задание
 
-public class FirstHomeWork {
+// Реализуйте структуру телефонной книги с помощью HashMap.
+// Программа также должна учитывать, что во входной структуре будут повторяющиеся имена с разными телефонами,
+// их необходимо считать, как одного человека с разными телефонами. Вывод должен быть отсортирован по убыванию числа телефонов.
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FirstHomeWork{
     private Map<String, ArrayList<String>> map = new HashMap<>();
 
     void add(String firstName, String telNum){
@@ -31,14 +41,45 @@ public class FirstHomeWork {
         return stringBuilder.toString();
     }
 
-    String getAll(){
-        // for (Map.Entry entry :
-        //     map.entrySet()) {
-        //     if(true){
-        //         System.out.println("Hello");
-        //     }
-
-        // }
-        return map.toString();
+    String getAllSorted(){
+        List<String> keysArrayList = new ArrayList<String>();
+        for (Map.Entry entry :
+                map.entrySet()) {
+            keysArrayList.add(entry.getKey().toString());
+            // System.out.println(entry.getKey().getClass().getName());
+        }
+        for (int i = 1; i < keysArrayList.size(); i++) {
+            for (int j = 0; j < keysArrayList.size(); j++) {
+                if(map.get(keysArrayList.get(i)).size() > map.get(keysArrayList.get(j)).size()){
+                    Collections.swap(keysArrayList, i, j);
+//                    System.out.println(keysArrayList.toString());
+                }
+            }
+        }
+        // ArrayList<Integer> valuesArrayList = new ArrayList<Integer>();
+        // for (String string : keysArrayList) {
+        //     valuesArrayList.add(map.get(string).size());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int k = 0; k <keysArrayList.size(); k++){
+            stringBuilder
+                    .append(keysArrayList.get(k))
+                    .append(" : ")
+                    .append(map.get(keysArrayList.get(k)))
+                    .append("\n");
+        }
+        return stringBuilder.toString();
     }
+    // return map.get("Гробушка");
+    // return map.toString();
 }
+// @Override
+// public int compareTo(FirstHomeWork o) {
+//     return 0;
+// }
+//    void sortMap(Map needToSortMap){
+//        maxLenght = 0;
+//        for (Map.Entry entry :
+//             map.entrySet()) {
+//            if(entry.)
+//    }
+//    }
--- src/taskHW.java
+++ src/taskHW.java
@@ -7,19 +7,27 @@ public class taskHW {
         firstHomeWork.add("Гробушка", "6349837923");
         firstHomeWork.add("Петро", "634983792123");
         firstHomeWork.add("Иван", "8888888");
-        firstHomeWork.add("Кирилл", "1123131251");
+        firstHomeWork.add("Тимон", "1123131251");
         firstHomeWork.add("Вадим", "634123579837923");
         firstHomeWork.add("Русичка", "6349837923123123");
         firstHomeWork.add("Ирусик", "634983787687923");
         firstHomeWork.add("Лёха", "11111111111");
-        firstHomeWork.add("Тимон", "2222");
+        firstHomeWork.add("Тимон", "2213222");
+        firstHomeWork.add("Пумба", "6349837923");
+        firstHomeWork.add("Гробушка", "68878989");
+        firstHomeWork.add("Гробушка", "123123512");
+        firstHomeWork.add("Гробушка", "14241246676");
+        firstHomeWork.add("Русичка", "99999999990899890");
+        firstHomeWork.add("Ирусик", "2222");
+        firstHomeWork.add("Гробушка", "11111111111");
+        firstHomeWork.add("Гробушка", "2222");
         firstHomeWork.add("Пумба", "6349837923");
         firstHomeWork.add("Гробушка", "68878989");
         firstHomeWork.add("Гробушка", "123123512");
         firstHomeWork.add("Гробушка", "14241246676");
 
         System.out.println(firstHomeWork.getByName("Гробушка"));
-        System.out.println(firstHomeWork.getByTel("11111111111"));
-        System.out.println(firstHomeWork.getAll());
+        System.out.println(firstHomeWork.getByTel("2222"));
+        System.out.println(firstHomeWork.getAllSorted());
     }
 }
